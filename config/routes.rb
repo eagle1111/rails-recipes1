001d673aed6718c9b45dcb0c819989062a2ec42f
eagle1111 <wang499817054@gmail.com>
2017-07-10 14:41:50 +0800

    bulk-update

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :events do
       resources :tickets, :controller => "event_tickets"
+
+      collection do
+        post :bulk_update
+      end
     end
     resources :users
     resources :groups
