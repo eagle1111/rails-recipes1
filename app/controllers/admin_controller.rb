5e946be4f720f5dd23f4845d8c1dbc8343dc00b2
eagle1111 <wang499817054@gmail.com>
2017-07-10 11:13:18 +0800

    多语言

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,4 +5,14 @@ class AdminController < ApplicationController
 
   layout "admin"
 
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locals.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
 end
